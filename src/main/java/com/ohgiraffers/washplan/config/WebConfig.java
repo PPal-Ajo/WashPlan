abd3711b4d7adf936162491cd500e415ec17237d
황순빈 <ghkdtnsqls04@gmail.com>
2024-12-12 16:37:01 +0900

    feature-MainFrontEnd-2024/12/12

---
+++ src/main/java/com/ohgiraffers/washplan/config/WebConfig.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.washplan.config;
+
+public class WebConfig {
+}
--- src/main/java/com/ohgiraffers/washplan/main/controller/MainController.java
+++ src/main/java/com/ohgiraffers/washplan/main/controller/MainController.java
@@ -1,4 +1,13 @@
 package com.ohgiraffers.washplan.main.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping({"/","/main"})
+    public String main() {
+        return "main/main";
+    }
 }
