2300bacdcf42dcaad5e07a46a2284e0f85840011
youngseok-1 <whdudtjr0@gmail.com>
2024-12-12 17:11:35 +0900

    main4

--- src/main/java/com/ohgiraffers/washplan/auth/controller/AuthController.java
+++ src/main/java/com/ohgiraffers/washplan/auth/controller/AuthController.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+
 public class AuthController {
 
+    @GetMapping("login")
+    public String loginPage() {
+        return "auth/login"; // templates/auth/login.html로 매핑
 
 
+    }
 }
-
 
 
