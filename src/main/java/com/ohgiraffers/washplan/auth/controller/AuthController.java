9e736deb067c34c4431ca2862d6cad54c09b142a
youngseok-1 <whdudtjr0@gmail.com>
2024-12-12 14:10:21 +0900

    feat:LoginFrontend/2024-12-12

--- src/main/java/com/ohgiraffers/washplan/auth/controller/AuthController.java
+++ src/main/java/com/ohgiraffers/washplan/auth/controller/AuthController.java
@@ -1,4 +1,13 @@
-package com.ohgiraffers.washplan.auth.controller;
-
-public class AuthController {
-}
+//    package com.ohgiraffers.washplan.auth.controller;
+//
+//    import org.springframework.stereotype.Controller;
+//    import org.springframework.web.bind.annotation.GetMapping;
+//
+//    @Controller
+//    public class AuthController {
+//
+//        @GetMapping("/login")
+//        public String loginpage() {
+//            return "auth/auth";
+//        }
+//    }
