39062210da1be4d861bade3cdba00dd4b9571e09
youngseok-1 <whdudtjr0@gmail.com>
2024-12-16 09:15:42 +0900

    feature/15-login-test

--- src/main/java/com/ohgiraffers/washplan/auth/model/service/AuthService.java
+++ src/main/java/com/ohgiraffers/washplan/auth/model/service/AuthService.java
@@ -2,6 +2,8 @@ package com.ohgiraffers.washplan.auth.model.service;
 
 import com.ohgiraffers.washplan.user.model.dao.UserMapper;
 import com.ohgiraffers.washplan.user.model.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -10,16 +12,23 @@ public class AuthService {
 
 
     private final UserMapper userMapper;
-    private final PasswordEncoder passwordEncoder;
 
-    public AuthService(UserMapper userMapper, PasswordEncoder passwordEncoder) {
+    @Autowired
+    public AuthService(UserMapper userMapper) {
         this.userMapper = userMapper;
-        this.passwordEncoder = passwordEncoder;
     }
 
     public boolean authenticate(String userId, String password) {
         UserDTO user = userMapper.findByUserId(userId);
-        return user != null && passwordEncoder.matches(password, user.getUserPwd());
+
+        // 사용자가 존재하고 비밀번호가 일치하는지 확인
+        if (user != null) {
+            PasswordEncoder encoder = new BCryptPasswordEncoder();
+            return encoder.matches(password, user.getUserPwd());
+        }
+        return false;
     }
+
+
 
 }
