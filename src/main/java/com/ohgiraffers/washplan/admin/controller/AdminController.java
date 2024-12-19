2153ae315963cd6ae1eb1c32618eec3301eba22e
황순빈 <ghkdtnsqls04@gmail.com>
2024-12-19 17:04:22 +0900

    feat:Admin-Front-End/2024-12-19

--- src/main/java/com/ohgiraffers/washplan/admin/controller/AdminController.java
+++ src/main/java/com/ohgiraffers/washplan/admin/controller/AdminController.java
@@ -1,4 +1,40 @@
 package com.ohgiraffers.washplan.admin.controller;
 
+
+import com.ohgiraffers.washplan.reservation.model.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AdminController {
+
+    private final ReservationService reservationService;
+
+    @Autowired
+    public AdminController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+
+        return "admin/admin";
+    }
+    @GetMapping("/adminUser")
+    public String adminUser(Model model) {
+
+        return "admin/adminUser";
+    }
+    @GetMapping("/adminMachine")
+    public String adminMachine(Model model) {
+
+        return "admin/adminMachine";
+    }
+    @GetMapping("/adminInquiry")
+    public String adminInquiry(Model model) {
+
+        return "admin/adminInquiry";
+    }
 }
