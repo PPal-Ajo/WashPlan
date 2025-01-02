91a91788c06e36374ed0ec732df7b926b77f0d3b
youngseok-1 <whdudtjr0@gmail.com>
2025-01-02 14:06:03 +0900

    feature/90-mypage-db/2025-01-02/3

--- src/main/java/com/ohgiraffers/washplan/user/controller/MyPageController.java
+++ src/main/java/com/ohgiraffers/washplan/user/controller/MyPageController.java
@@ -1,11 +1,19 @@
 package com.ohgiraffers.washplan.user.controller;
 
+import com.ohgiraffers.washplan.auth.model.dto.CustomUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyPageController {
 
     @GetMapping("/mypage")
-    public String myPage() { return "mypage/mypage";}
+    public String myPage(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+
+        model.addAttribute("username", userDetails.getUsername());
+        model.addAttribute("email", userDetails.getEmail());
+
+        return "mypage/mypage";}
 }
