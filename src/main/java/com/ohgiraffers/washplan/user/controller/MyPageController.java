9e8581af0c20c226550f3fee784225643f42ed59
youngseok-1 <whdudtjr0@gmail.com>
2025-01-02 10:48:50 +0900

    feature/88-mypage-front/2025-01-02

--- src/main/java/com/ohgiraffers/washplan/user/controller/MyPageController.java
+++ src/main/java/com/ohgiraffers/washplan/user/controller/MyPageController.java
@@ -1,4 +1,11 @@
 package com.ohgiraffers.washplan.user.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MyPageController {
+
+    @GetMapping("/mypage")
+    public String myPage() { return "mypage/mypage";}
 }
