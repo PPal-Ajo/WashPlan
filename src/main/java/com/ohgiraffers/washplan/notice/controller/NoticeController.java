c1cce30400f39bf447c386c2ee459599c3d5d54f
youngseok-1 <whdudtjr0@gmail.com>
2025-01-14 17:02:44 +0900

    feature/135-notice-front

--- src/main/java/com/ohgiraffers/washplan/notice/controller/NoticeController.java
+++ src/main/java/com/ohgiraffers/washplan/notice/controller/NoticeController.java
@@ -1,4 +1,25 @@
 package com.ohgiraffers.washplan.notice.controller;
 
+import com.ohgiraffers.washplan.notice.model.dto.NoticeDTO;
+import com.ohgiraffers.washplan.notice.model.service.NoticeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import java.util.List;
+
+@Controller
 public class NoticeController {
+
+    private final NoticeService noticeService;
+
+    public NoticeController(NoticeService noticeService) {
+        this.noticeService = noticeService;
+    }
+
+    @GetMapping("/notice")
+    public String noticePage(Model model) {
+        List<NoticeDTO> notices = noticeService.getAllNotices();
+        model.addAttribute("notices", notices);
+        return "faq/notice";
+    }
 }
--- src/main/java/com/ohgiraffers/washplan/notice/model/service/NoticeService.java
+++ src/main/java/com/ohgiraffers/washplan/notice/model/service/NoticeService.java
@@ -1,4 +1,21 @@
 package com.ohgiraffers.washplan.notice.model.service;
 
+import com.ohgiraffers.washplan.notice.model.dao.NoticeMapper;
+import com.ohgiraffers.washplan.notice.model.dto.NoticeDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class NoticeService {
+    
+    private final NoticeMapper noticeMapper;
+    
+    public NoticeService(NoticeMapper noticeMapper) {
+        this.noticeMapper = noticeMapper;
+    }
+    
+    public List<NoticeDTO> getAllNotices() {
+        return noticeMapper.findAllNotices();
+    }
 }
